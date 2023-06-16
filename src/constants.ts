09963f3f7eec7ea83b2d2b55d40b2442d0d3852a
sundaydavid <sundayudoekong1@gmail.com>
2023-06-16 15:49:48 +0100

    Mini Spotify

---
+++ src/constants.ts
@@ -0,0 +1,49 @@
+import { Track } from "react-native-track-player";
+
+export const playListData: Track[] = [
+    {
+      id: 1,
+      title: 'Amapiano Music',
+      artist: 'Asake',
+      album: 'Champagne Talk',
+      artwork:
+        'https://i0.wp.com/xclusiveloaded.com/wp-content/uploads/2023/05/Asake-Amapiano-ft-Olamide.webp?fit=750%2C750&ssl=1',
+      url: require('./assets/asake.mp3'),
+    },
+    {
+      id: 2,
+      title: 'Ojapiano',
+      artist: 'Kcee',
+      album: 'Shershaah',
+      artwork:
+        'https://i0.wp.com/xclusiveloaded.com/wp-content/uploads/2023/06/Kcee-%E2%80%93-Ojapiano.webp?fit=592%2C592&ssl=1',
+      url: require('./assets/kcee.mp3'),
+    },
+    {
+      id: 3,
+      title: 'On My Own',
+      artist: 'Kaptain',
+      album: 'Brahmastra',
+      artwork:
+        'https://i0.wp.com/xclusiveloaded.com/wp-content/uploads/2022/06/Kaptain-%E2%80%93-Beyond-Any-Reasonable-Doubt-EP.webp?fit=720%2C404&ssl=1',
+      url: require('./assets/onmyown.mp3'),
+    },
+    {
+      id: 4,
+      title: 'Missing',
+      artist: 'Kaptain',
+      album: 'Pal Pal Dil Ke Paas',
+      artwork:
+      'https://i0.wp.com/xclusiveloaded.com/wp-content/uploads/2022/06/Kaptain-%E2%80%93-Beyond-Any-Reasonable-Doubt-EP.webp?fit=720%2C404&ssl=1',
+      url: require('./assets/missing.mp3'),
+    },
+    {
+      id: 5,
+      title: 'Besharam Rang',
+      artist: 'Vishal & Shekhar, Shilpa Rao',
+      album: 'Pathaan',
+      artwork:
+        'https://c.saavncdn.com/807/Pathaan-Hindi-2022-20221222104158-500x500.jpg',
+      url: require('./assets/Employer.mp3'),
+    },
+  ];
